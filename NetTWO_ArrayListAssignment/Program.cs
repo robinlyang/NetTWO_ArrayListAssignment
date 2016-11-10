3072be8e503b9f57ebb8835e8d3958f0277aec03
robinlyang <robiny@gmail.com>
2016-11-09 19:13:28 -0700

    Initial Project

--- NetTWO_ArrayListAssignment/Program.cs
+++ NetTWO_ArrayListAssignment/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace NetTWO_ArrayListAssignment
 {
@@ -10,6 +11,29 @@ namespace NetTWO_ArrayListAssignment
     {
         static void Main(string[] args)
         {
+            ArrayList col = new ArrayList(1);
+            string obj = "";
+            Console.WriteLine("Enter a name or number");
+            do
+            {
+                obj = Console.ReadLine();
+                if(obj == "")
+                {
+                    obj = null;
+                }
+                if(obj != null)
+                {
+                    col.Add(obj);
+                }
+            } while (obj != null);
+            Console.WriteLine("Size: " + col.Count);
+            col.Sort();
+            for(int x = 0; x < col.Count; x++)
+            {
+                Console.WriteLine(col[x]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
